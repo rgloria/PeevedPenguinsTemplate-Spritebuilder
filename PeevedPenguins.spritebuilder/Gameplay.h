066b544443af08598ffe49b3aece799486283f58
Rodrigo Gloria <rgloria@Rodrigos-MacBook-Air.local>
2014-07-01 19:42:57 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.h
@@ -0,0 +1,17 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Rodrigo Gloria on 7/1/14.
+//  Copyright 2014 Apportable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface Gameplay : CCNode {
+    CCPhysicsNode *_physicsNode;
+    CCNode *_catapultArm;
+}
+
+@end
---
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -0,0 +1,14 @@
+//
+//  Gameplay.m
+//  PeevedPenguins
+//
+//  Created by Rodrigo Gloria on 7/1/14.
+//  Copyright 2014 Apportable. All rights reserved.
+//
+
+#import "Gameplay.h"
+
+
+@implementation Gameplay
+
+@end
